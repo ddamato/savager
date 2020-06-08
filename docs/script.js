5f7ba5c18161e2734935d65c86f3f6b6afcb4321
Donnie D'Amato <fauxserious@gmail.com>
2020-06-08 13:38:05 -0400

    Rearrange assets for examples

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,12 @@
-import savagerSymbols from './svg/symbols.js';
+import savagerSymbols from './assets/symbols.js';
 import Savager from './savager/savager.esm.js';
 
-const options =  { classNames: 'savager-svg', toSvgElement: true, consolidate: false, autoAppend: true };
+const options = { classNames: 'savager-svg', toSvgElement: true, consolidate: false, autoAppend: true };
 const savager = new Savager(savagerSymbols, options);
 const { assets, sheet, inject } = savager.prepareAssets(['bang-triangle', 'clock-reverse']);
 
 if (sheet && !options.autoAppend) {
-  //document.body.appendChild(sheet);
+  document.body.appendChild(sheet);
 }
 
 inject();
