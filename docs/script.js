2058ae25b85dd45ee60ffc7fa8e5ece427267b5b
Donnie D'Amato <fauxserious@gmail.com>
2020-06-12 06:10:15 -0400

    Add logo, fixture

--- docs/script.js
+++ docs/script.js
@@ -1,15 +1,26 @@
 import savagerSymbols from './assets/symbols.js';
 import Savager from './savager/savager.esm.js';
 
-const options = { classNames: 'savager-svg', toSvgElement: true, consolidate: false, autoAppend: true };
-const savager = new Savager(savagerSymbols, options);
-const { assets, sheet, inject } = savager.prepareAssets(['bang-triangle', 'clock-reverse']);
+const options = { classNames: 'savager-svg', toSvgElement: true, autoAppend: true };
+const savager = new Savager(savagerSymbols);
+
+const pageAssets = savager.prepareAssets('savager-logo', { toSvgElement: true, autoAppend: true });
+const logo = pageAssets.assets['savager-logo'].cloneNode(true);
+logo.firstElementChild.classList.add('savager-logo');
+
+const h1 = document.querySelector('h1');
+
+h1.appendChild(logo);
+
+const { assets, sheet, inject } = savager.prepareAssets(['bang-triangle', 'clock-reverse'], options);
 
 if (sheet && !options.autoAppend) {
   document.body.appendChild(sheet);
 }
 
-inject();
+if (typeof inject === 'function') {
+  inject();
+}
 
 const inventory = document.querySelector('.inventory');
 Object.entries(assets).forEach(([name, elem]) => {
