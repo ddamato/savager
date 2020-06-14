cb17172a98cf0c14e43cf7805b0a287909c4d610
Donnie D'Amato <fauxserious@gmail.com>
2020-06-14 12:54:26 -0400

    Finish doc styles

--- bin/build-docs.js
+++ bin/build-docs.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const fs = require('fs-extra'); 
 const nunjucks = require('nunjucks');
+const { load } = require('cheerio');
 const md = require('markdown-it')({
   html: true,
   linkify: true,
 });
 const mdPrism = require('markdown-it-prism');
+const mdAnchor = require('markdown-it-anchor');
 const { minify } = require('html-minifier');
 
-md.use(mdPrism);
+md.use(mdPrism).use(mdAnchor);
 
 const DOCS_DIR = path.resolve(__dirname, '..', 'docs');
 
@@ -22,7 +24,11 @@ async function buildDocs() {
   const index = path.join(DOCS_DIR, 'index.html');
   const markdown = await fs.readFile(path.join(DOCS_DIR, 'index.md'));
   const content = md.render(markdown.toString());
-  const html = env.render('index.njk', { content });
+  const $ = load(content);
+  const navigation = $('h2').map(function () { 
+      return { text: $(this).text(), href: $(this).attr('id') }
+   }).get();
+  const html = env.render('index.njk', { content, navigation });
   fs.ensureFileSync(index);
   fs.writeFileSync(index, minify(html, { collapseWhitespace: true }), { encoding: 'utf8' });
 }
--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,6 @@
 import savagerSymbols from './assets/symbols.js';
 import Savager from './savager/savager.esm.js';
 
-const options = { classNames: 'savager-svg', toSvgElement: true, autoAppend: true };
 const savager = new Savager(savagerSymbols);
 
 const pageAssets = savager.prepareAssets('savager-logo', { toSvgElement: true, autoAppend: true });
@@ -9,23 +8,5 @@ const logo = pageAssets.assets['savager-logo'].cloneNode(true);
 logo.firstElementChild.classList.add('savager-logo');
 
 const h1 = document.querySelector('h1');
-
+h1.innerText = '';
 h1.appendChild(logo);
-
-const { assets, sheet, inject } = savager.prepareAssets(['bang-triangle', 'clock-reverse'], options);
-
-if (sheet && !options.autoAppend) {
-  document.body.appendChild(sheet);
-}
-
-if (typeof inject === 'function') {
-  inject();
-}
-
-const inventory = document.querySelector('.inventory');
-Object.entries(assets).forEach(([name, elem]) => {
-  const li = document.createElement('li');
-  inventory.appendChild(li);
-  li.dataset.name = name;
-  li.appendChild(elem);
-});
